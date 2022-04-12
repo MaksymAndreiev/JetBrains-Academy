3c4aadd46b14b4f0844be38183ea6cf581ea2d46
Maksym Andreiev <maxoggyha@gmail.com>
2022-04-12 22:50:20 +0200

    Simple Chatty Bot add stages

---
+++ Simple Chatty Bot/2/bot.py
@@ -0,0 +1,7 @@
+print('Hello! My name is Aid.')
+print(f'I was created in 2022.')
+print('Please, remind me your name.')
+
+# reading a name
+your_name = input()
+print(f'What a great name you have, {your_name}!')
